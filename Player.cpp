8b96b6ae2b3d62f5e416925675c6bb65ba8067a1
ggn <dev@ggngnn.net>
2023-05-04 16:42:11 +0200

    fast commit

--- Player.cpp
+++ Player.cpp
@@ -2,12 +2,58 @@
 // Created by indianajones on 4/29/23.
 //
 
+#include <SFML/Graphics.hpp>
+#include <string>
 #include "Player.h"
+#include <iostream>
 
-void Player::move() {
+using namespace std;
 
+Player::Player(string& namearg, sf::RenderWindow & windowarg): Player{namearg, windowarg, 3}
+{
 }
 
-void Player::texture() {
+Player::Player(string& namearg, sf::RenderWindow & windowarg, int hparg):name(namearg), window{windowarg}, hp(hparg)
+{
+}
 
+void Player::move(bool direction)
+{
+    int px;
+    bool visible;
+    if(direction)
+    {
+        px = 10;
+        visible=window.getSize().x-playerSprite.getPosition().x-px-playerTexture.getSize().x>0;
+    }
+    else
+    {
+        px = -10;
+        visible=(playerSprite.getPosition().x+px)>=0;
+    }
+    if(visible)
+        playerSprite.setPosition(playerSprite.getPosition().x+px,playerSprite.getPosition().y);
+    showSprite();
+}
+
+void Player::loadTexture()
+{
+    playerTexture.loadFromFile("../poppy.png");
+    playerSprite.setTexture(playerTexture);
+    playerSprite.setPosition(250,300);
+}
+
+void Player::showSprite()
+{
+    window.draw(playerSprite);
+}
+
+int Player::getHP() const
+{
+    return hp;
+}
+
+string Player::getName() const
+{
+    return name;
 }
--- Player.h
+++ Player.h
@@ -5,11 +5,25 @@
 #ifndef SPACE_SHOOTER_PLAYER_H
 #define SPACE_SHOOTER_PLAYER_H
 
+class Player
+{
+    public:
+        explicit Player(std::string &, sf::RenderWindow &);
+        Player(std::string &, sf::RenderWindow &, int);
 
-class Player {
-public:
-    void move();
-    void texture();
+        void move(bool direction);
+        void loadTexture();
+        void showSprite();
+
+        int getHP() const;
+        std::string getName() const;
+
+    private:
+        sf::RenderWindow & window;
+        const std::string name;
+        int hp;
+        sf::Texture playerTexture;
+        sf::Sprite playerSprite;
 };
 
 
