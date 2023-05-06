2320813e7d8383176749879fa9e2f1fd4e0342b0
indianajones <klintmikolaj@gmail.com>
2023-05-06 14:53:28 -0400

    merged bart with main

--- Menu.h
+++ Menu.h
@@ -1,21 +1,22 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "Game.h"
-#define MENU_ITEMS 4
+#define MENU_ITEMS 5
 using namespace std;
 using namespace sf;
 
 class Menu {
 public:
-    Menu(float x, float y);
-    void draw(RenderWindow &window);
-    void menu_update(RenderWindow &window);
+    Menu(float x, float y, RenderWindow &);
+//    void draw(RenderWindow &window);
+    void draw();
+    int menu_update();
     void Move_Up();
     void Move_Down();
 private:
+    RenderWindow &window;
     int Selected_index;
     Font font;
     Text menu[MENU_ITEMS];
-
 };
 
--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Game.h"
-#include "Player.h"
+#include "Audio.h"
+#include "Menu.h"
 
 using namespace std;
+using namespace sf;
 
 int main()
 {
-    const int windowWidth=1000;
-    const int windowHeight=800;
     string name="adam";
-    sf::RenderWindow window;
-    sf::Font font;
-    font.loadFromFile("../ethnocentricRg.otf");
-    window.create(sf::VideoMode(windowWidth, windowHeight), "Space shooter (name subject to change)");
+    RenderWindow window;
+    window.create(VideoMode(1000, 800), "Space shooter (name subject to change)");
+    Menu menu(window.getSize().x, window.getSize().y, window);
+    menu.menu_update();
     Player player(name, window);
-    Game game(player, window, font);
+    Game game(player, window);
     game.run();
+
     return 0;
 }
