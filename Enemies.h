31de43316602a7caf5ed7b747bb5b39f9e716b0d
indianajones <klintmikolaj@gmail.com>
2023-05-07 14:25:33 -0400

    Enemies poczatek

--- Enemies.h
+++ Enemies.h
@@ -4,9 +4,16 @@ using namespace sf;
 
 class Enemies {
 public:
+    Enemies(RenderWindow &);
 
+    void loadTexture();
+    void showSprite();
+
+    int get_enemy_HP() const;
 private:
     int hp;
     int hpMAX;
+    Texture asteroid_Texture;
+    Sprite asteroid_Sprite;
 };
 
