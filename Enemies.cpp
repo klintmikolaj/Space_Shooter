03413ba9d69d5aeadece39e82aaacf6d7b87646c
indianajones <klintmikolaj@gmail.com>
2023-05-07 14:25:33 -0400

    Enemies poczatek

--- Enemies.cpp
+++ Enemies.cpp
@@ -1,1 +1,16 @@
 #include "Enemies.h"
+
+//Enemies::Enemies(RenderWindow & windowarg): window{windowarg} {
+//}
+//
+//void Enemies::loadTexture() {
+//
+//}
+//
+//void Enemies::showSprite() {
+//    window.draw(asteroid_Sprite);
+//}
+//
+//int Enemies::get_enemy_HP() const {
+//    return hp;
+//}
