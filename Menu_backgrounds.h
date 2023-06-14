ec8280aacb4ec2552b21e6d8e12365301f5254b0
indianajones <klintmikolaj@gmail.com>
2023-06-14 12:02:08 -0400

    hud color

--- Menu_backgrounds.h
+++ Menu_backgrounds.h
@@ -10,8 +10,20 @@
 class Menu_backgrounds {
 public:
     Menu_backgrounds(sf::RenderWindow &window);
+
+    /*
+     * loading textures from file
+     */
     void setBackgroundTexture();
+
+    /*
+     * @param credits
+     * Adding audio sounds
+     * Setting background texture
+     * Displaying menu
+     */
     void draw(bool credits);
+
 private:
     Audio audio;
     sf::RenderWindow &window;
